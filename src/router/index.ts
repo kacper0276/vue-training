5224c1dbee8900bda9a9668fd801147f0fe12dcb
kacper0276 <kacper0276@op.pl>
2024-08-14 00:02:04 +0200

    Routing config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,23 @@
-import { createRouter, createWebHistory } from "vue-router";
+import About from "@/components/routesComponents/About.vue";
+import Home from "@/components/routesComponents/Home.vue";
+import NotFound from "@/components/routesComponents/NotFound.vue";
+import Projects from "@/components/routesComponents/Projects.vue";
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from "vue-router";
+
+const routes: RouteRecordRaw[] = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+  { path: "/projects", component: Projects },
+  { path: "/:pathMatch(.*)*", component: NotFound }, // Obsługa nieznanych ścieżek
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 });
 
 export default router;
