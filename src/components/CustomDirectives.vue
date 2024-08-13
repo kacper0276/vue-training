d7755d3e96eafa29ad275a4751d20e9909dbf10d
kacper0276 <kacper0276@op.pl>
2024-08-14 00:02:45 +0200

    Custom directives

---
+++ src/components/CustomDirectives.vue
@@ -0,0 +1,27 @@
+<template>
+  <!--
+        Niestandardowe dyrektywy w Vue są używane do wykonywania 
+        niskopoziomowych operacji bezpośrednio na elementach DOM. 
+        Są one szczególnie przydatne, gdy chcemy wykonać operacje, 
+        które nie są bezpośrednio związane z danymi komponentu, 
+        takie jak manipulacja atrybutami elementów, dodawanie 
+        nasłuchiwaczy zdarzeń itp.
+    -->
+
+  <div>
+    <input v-focus type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    focus: {
+      // hook jak DOM jest gotowy
+      mounted(el) {
+        el.focus();
+      },
+    },
+  },
+};
+</script>
