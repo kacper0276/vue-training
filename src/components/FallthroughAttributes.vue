7322eb2c4d68aefa25cbab29d12c8c9dc92cf9ee
kacper0276 <kacper0276@op.pl>
2024-08-13 04:12:25 +0200

    Create FallthroughAttributes

---
+++ src/components/FallthroughAttributes.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- 
+        Przekazujemy atrybuty `type` i `disabled` do komponentu CustomButton 
+        Te atrybuty nie są zdefiniowane jako `props` w komponencie CustomButton, 
+        więc są traktowane jako atrybuty przepuszczające tzw Fallthrough Attributes
+        -->
+
+    <FallthroughAttributesCustomButton type="submit" disabled>
+      Wyślij dane na serwer
+    </FallthroughAttributesCustomButton>
+  </div>
+</template>
+
+<script>
+import FallthroughAttributesCustomButton from "./FallthroughAttributesCustomButton.vue";
+
+export default {
+  components: {
+    CustomButton,
+  },
+};
+</script>
---
+++ src/components/FallthroughAttributesCustomButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- 
+      Główny element korzenia komponentu (button) 
+      otrzymuje wszystkie atrybuty przepuszczające 
+
+      Komponent CustomButton nie definiuje żadnych props, 
+      więc wszystkie atrybuty przekazane do niego są traktowane 
+      jako atrybuty przepuszczające.  
+    -->
+  <button>
+    <slot></slot>
+    <!-- w miejsce slot wejdzie tekst "Wyślij dane na serwer" -->
+  </button>
+</template>
+
+<script>
+export default {
+  name: "FallthroughAttributesCustomButton",
+};
+</script>
