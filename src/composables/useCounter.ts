f1caf061ba8934c3f82496a2a15ca2d9fa7fd245
kacper0276 <kacper0276@op.pl>
2024-08-14 00:01:27 +0200

    Create composable

---
+++ src/composables/useCounter.ts
@@ -0,0 +1,19 @@
+import { ref } from "vue";
+
+export default function useCounter() {
+  const count = ref(0);
+
+  const increment = () => {
+    count.value++;
+  };
+
+  const decrement = () => {
+    count.value--;
+  };
+
+  return {
+    count,
+    increment,
+    decrement,
+  };
+}
