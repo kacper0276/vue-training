02e2dad5bc426c369568b641cda0ddc696c8e6e5
kacper0276 <kacper0276@op.pl>
2024-08-13 04:10:30 +0200

    Declare components

--- components.d.ts
+++ components.d.ts
@@ -14,5 +14,17 @@ declare module "@vue/runtime-core" {
     BaseLayout: typeof import("./src/components/BaseLayout.vue")["default"];
     ComputedProperties: typeof import("./src/components/ComputedProperties.vue")["default"];
     TwoWayBinding: typeof import("./src/components/TwoWayBinding.vue")["default"];
+    ManyTwoWayBinding: typeof import("./src/components/ManyTwoWayBinding.vue")["default"];
+    ClassStyleBinding: typeof import("./src/components/ClassStyleBinding.vue")["default"];
+    HooksLifeCycle: typeof import("./src/components/HooksLifeCycle.vue")["default"];
+    Ref: typeof import("./src/components/Ref.vue")["default"];
+    Watcher: typeof import("./src/components/Watcher.vue")["default"];
+    AsyncComponent: typeof import("./src/components/AsyncComponent.vue")["default"];
+    AsyncComponentChild: typeof import("./src/components/AsyncComponentChild.vue")["default"];
+    FallthroughAttributes: typeof import("./src/components/FallthroughAttributes.vue")["default"];
+    FallthroughAttributesCustomButton: typeof import("./src/components/FallthroughAttributesCustomButton.vue")["default"];
+    ProvideInject: typeof import("./src/components/ProvideInject.vue")["default"];
+    ProvideInjectChildComponent: typeof import("./src/components/ProvideInjectChildComponent.vue")["default"];
+    ProvideInjectMiddleComponent: typeof import("./src/components/ProvideInjectMiddleComponent.vue")["default"];
   }
 }
