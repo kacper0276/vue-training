0117e8d74f63a6f162453bf9980e2eccdbd15407
kacper0276 <kacper0276@op.pl>
2024-08-13 04:13:00 +0200

    Create ManyTwoWayBinding

---
+++ src/components/ManyTwoWayBinding.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div>
+      <input
+        type="text"
+        :value="name"
+        @input="(event) => $emit('update:name', (event.target as HTMLInputElement).value)"
+        placeholder="Wpisz imię"
+      />
+      <!-- 
+                W Vue, $event to specjalna zmienna, która 
+                odnosi się do natywnego zdarzenia DOM 
+                wywołanego przez użytkownika lub 
+                przez przeglądarkę
+            -->
+    </div>
+
+    <div>
+      <input
+        type="text"
+        :value="surname"
+        @input="(event) => $emit('update:surname', (event.target as HTMLInputElement).value)"
+        placeholder="Wpisz nazwisko"
+      />
+    </div>
+
+    <label>
+      <input
+        type="checkbox"
+        :checked="accepted"
+        @change="(event) => $emit('update:accepted', (event.target as HTMLInputElement).checked)"
+      />
+      Akceptuję
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+    surname: {
+      type: String,
+      default: "",
+    },
+    accepted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style></style>
