7454b3cdc234962ad303db8864ee2daab9bcfcfa
kacper0276 <kacper0276@op.pl>
2024-08-15 15:24:49 +0200

    Use vuex store

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+// @ts-ignore
+import store from "./stores/store";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(store);
 
 app.mount("#app");
